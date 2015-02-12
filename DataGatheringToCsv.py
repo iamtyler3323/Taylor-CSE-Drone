fadf94d86d2141fafb81199f5557ace061971335
unknown <tgarcia@drone-room-pc1.cs.local>
2015-02-11 21:27:33 -0500

    Data gathering updated

--- DataGatheringToCsv.py
+++ DataGatheringToCsv.py
@@ -3,9 +3,9 @@ import os
 import os.path
 import platform
 
-target_data_count = 500;
+target_data_count = 10;
 def get():
-    return cs.climbrate
+    return cs.alt
 
 
 userhome = os.path.expanduser('~')
@@ -14,7 +14,7 @@ desktop = "//fileshare/tgarcia/Desktop/"
 count = 0
 
 #Change to what you want your file to be called, otherwise you will append to whatever is here
-file_name = desktop + 'dataTest.csv'
+file_name = desktop + 'dataTestAlt.csv'
 print(file_name +"Exists? " + str(os.path.exists(file_name)))
 if os.path.isfile(file_name):
     print("File exists")
@@ -26,7 +26,7 @@ else:
     with open(file_name,'rb') as f:
         data = list(csv.reader(f))
 
-f = open(file_name,"a")
+f = open(file_name,"wb")
 import collections
 counter = collections.defaultdict(int)
 for row in data:
@@ -35,8 +35,8 @@ for row in data:
 writer = csv.writer(f)
 while count < target_data_count:
     count += 1
-    writer.writerow("{0}\t{1}".format(count,get()))
-    print("{0}\t{1}".format(get(),count))
+    writer.writerow(["{0}".format(get())])
+    #print("{0}\t{1}".format(get(),count))
     Script.Sleep(100)
                     
 f.close()
