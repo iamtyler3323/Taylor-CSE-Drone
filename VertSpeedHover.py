e52b644e4fe1d9e3b3bda8b7fa3f025c70ae1113
Tyler Garcia <iamtyler3323@gmail.com>
2015-04-13 18:51:03 -0400

    Vert Speed update.

--- VertSpeedHover.py
+++ VertSpeedHover.py
@@ -9,7 +9,21 @@ class VertSpeed:
         self.hover.update(cs.alt,cs.verticalspeed)
         if self.hover.get_direction() == "UP":
             if self.hover.get_target() < self.hover.get_alt():
+                if self.debug:
+                    print("Updating throttle...\n\tOld throttle: {}".format(self.hover.throttle))
                 self.hover.throttle -= self.hover.get_speed()
+                if self.debug:
+                    print("New throttle: {}".format(self.hover.throttle))
             else:
-                if self.debug is True:
+                if self.debug:
                     print("Going up, keep her steady!\n\tAltitude: {0}\n\tDirection: {1}\n\tThrottle: {2}".format(self.hover.get_alt(),self.hover.get_direction(),self.hover.throttle))
+        elif self.hover.get_direction() == "DOWN":
+            if self.hover.get_target() > self.hover.get_alt():
+                if self.debug:
+                    print("Updating throttle...\n\tOld throttle: {}".format(self.hover.throttle))
+                self.hover.throttle += self.hover.get_speed()
+                if self.debug:
+                    print("New throttle: {}".format(self.hover.throttle))
+            else:
+                if self.debug:
+                    print("Going down, keep her steady!\n\tAltitude: {0}\n\tDirection: {1}\n\tThrottle: {2}".format(self.hover.get_alt(),self.hover.get_direction(),self.hover.throttle))
