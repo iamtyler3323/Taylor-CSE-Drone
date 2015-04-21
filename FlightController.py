51ddf0c56833e7cefe1cde0ebb61cc74b29e89a9
tgarcia <iamtyler3323@gmail.com>
2015-04-20 20:50:23 -0400

    Updated CSFake to add more fake variables Updated flight controller to do some tests Updated vertical speed hover to actually work Renamed get_direction to get_direction_vertspeed in HoverClass

--- FlightController.py
+++ FlightController.py
@@ -1,6 +1,8 @@
 __author__ = 'Tyler'
 from HoverClass import *
 from VertSpeedHover import *
+from CSFake import *
+from random import *
 
 # Hover types
 #   Vertical Speed hover: 0
@@ -9,7 +11,15 @@ from VertSpeedHover import *
 
 hover_type = 0
 
-hover_class = Hover(1.0, 1500)
-
+hover_class = Hover(10.0, 1500)
+cs = cs()
 if hover_type == 0:
     hover = VertSpeed(hover_class, True)
+
+
+for i in range(0,100):
+    print("Run {}".format(i))
+    cs.alt = randint(0,20)
+    cs.verticalspeed = randint(-3,3)
+    print("\tAlt: {0} \n\tVertical Speed: {1}".format(cs.alt, cs.verticalspeed))
+    hover.update(cs)
